b25e6aa4282ec55789c7d80ec1b7c0ea63515e05
Fagner Caetano <caetanofagner29@gmail.com>
2025-04-10 14:04:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Link de acesso no navegador - http://127.0.0.1:8000
 @app.get("/")
-async def root():
+def root():
+    """Retorna uma mensagem de boas-vindas."""
     return {"message": "Hello World"}
-
-# Link de acesso no navegador - http://127.0.0.1:8000/teste1
-@app.get("/teste1")
-async def root():
-    return {"Funcinou"}
-
